0a78c7bcc7f3321e71a2894ca279d924f2bafa26
J. Alex Mina <juanmina1200@gmail.com>
2021-09-09 12:20:54 -0400

    Update menus.js
    accidentally capitalized true and broke website for a few minutes

--- src/menus.js
+++ src/menus.js
@@ -126,7 +126,8 @@ window.menus = {
 	[localize("&Labs")]: [
 		{
 			item: localize("&Local Setup"),
-			enabled: false,
+			enabled: true,
+			action: ()=> { window.open("https://github.com/cs123tas/labs/tree/master/lab00"); },
 			description: localize("Open Local Setup lab."),
 		},
 		{
