4763051c8a9b6a738da9dcaf81a29930a7f22e38
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2021-09-28 12:15:45 -0400

    Post lecture recording.

--- js/lectures.js
+++ js/lectures.js
@@ -71,8 +71,8 @@
       date: '9/28',
       topic: 'Camera models & viewing transforms',
        links: {
-         'PPTX': 'https://docs.google.com/file/d/1XdaXiQYPNUpG4IapIEb6_6UzW-tK4Ehc/edit?usp=sharing&ouid=114892003088852070829&rtpof=true&sd=true'
-       	//  'Recording': 'https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=2ae39cde-16b1-4fe3-a349-ac4601242408',
+         'PPTX': 'https://docs.google.com/file/d/1XdaXiQYPNUpG4IapIEb6_6UzW-tK4Ehc/edit?usp=sharing&ouid=114892003088852070829&rtpof=true&sd=true',
+       	 'Recording': 'https://brown.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=abca5da9-211d-4fb8-bc57-ad9c013c82a6',
    	// //Originally Shapes and IP 1 today, but Spike changed plans.
        },
      },
